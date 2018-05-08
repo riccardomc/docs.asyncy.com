92199654dba286a43feb46ff13331a2e905eec39
Andrea Law <abylaw@gmail.com>
2018-05-08 12:14:12 -0400

    Add github contribute links

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,9 @@ module.exports = {
   title: 'Asyncy Docs',
   description: '',
   themeConfig: {
+    repo: 'asyncy/docs.asyncy.com',
+    repoLabel: 'Contribute!',
+    editLinks: true,
     sidebar: [
       '/',
       '/storyscript',
