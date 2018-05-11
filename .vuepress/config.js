0c08b0bddd77348f6846830fe25ca85b31ed2499
Andrea Law <abylaw@gmail.com>
2018-05-10 21:31:23 -0400

    Asyncy theming - first pass

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,6 +6,13 @@ module.exports = {
     repo: 'asyncy/docs.asyncy.com',
     repoLabel: 'Contribute!',
     editLinks: true,
+    nav: [
+      { text: 'Platform', link: 'https://5af1a22e1f12b77e9ad384c2--asyncy-homepage.netlify.com/platform' },
+      { text: 'Documentation', link: '/' },
+      { text: 'About', link: 'https://5af1a22e1f12b77e9ad384c2--asyncy-homepage.netlify.com/about' },
+      { text: 'Events', link: 'https://5af1a22e1f12b77e9ad384c2--asyncy-homepage.netlify.com/events' },
+      { text: 'Contact', link: 'https://5af1a22e1f12b77e9ad384c2--asyncy-homepage.netlify.com/contact' }
+    ],
     sidebar: [
       '/',
       {
---
+++ .vuepress/theme/Navbar.vue
@@ -0,0 +1,66 @@
+<template>
+  <header class="navbar">
+    <SidebarButton @toggle-sidebar="$emit('toggle-sidebar')"/>
+    <router-link :to="$localePath" class="home-link">
+      <img class="logo" :src="logo">
+    </router-link>
+    <div class="links">
+      <AlgoliaSearchBox v-if="isAlgoliaSearch" :options="algolia"/>
+      <SearchBox v-else-if="$site.themeConfig.search !== false"/>
+      <NavLinks class="can-hide"/>
+    </div>
+  </header>
+</template>
+
+<script>
+import SidebarButton from './SidebarButton.vue'
+import AlgoliaSearchBox from '@AlgoliaSearchBox'
+import SearchBox from './SearchBox.vue'
+import NavLinks from './NavLinks.vue'
+
+import logo from './images/logo.svg'
+
+export default {
+  components: { SidebarButton, NavLinks, SearchBox, AlgoliaSearchBox },
+  data() {
+    return {
+      logo: logo,
+    };
+  },
+  computed: {
+    algolia () {
+      return this.$themeLocaleConfig.algolia || this.$site.themeConfig.algolia || {}
+    },
+    isAlgoliaSearch () {
+      return this.algolia && this.algolia.apiKey && this.algolia.indexName
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+@import './styles/config.styl'
+
+.navbar
+  padding 1.9rem 1.5rem
+  line-height $navbarHeight - 1.4rem
+  position relative
+  a, span, img
+    display inline-block
+  .logo
+    height $navbarHeight - 3.9rem
+    min-width $navbarHeight - 2.6rem
+    margin-right 0.8rem
+    vertical-align top
+  .links
+    font-size 0.9rem
+    position absolute
+    right 1.5rem
+    top 0.7rem
+
+@media (max-width: $MQMobile)
+  .navbar
+    padding-left 4rem
+    .can-hide
+      display none
+</style>
